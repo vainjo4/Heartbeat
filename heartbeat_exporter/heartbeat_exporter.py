09368b32c2c1d27f755a221e1fddf518bc4ade8c
Johannes Vainio <vainjo4@gmail.com>
2020-06-25 15:13:59 +0300

    Exporter evaluates

--- heartbeat_exporter/heartbeat_exporter.py
+++ heartbeat_exporter/heartbeat_exporter.py
@@ -1,5 +1,7 @@
 import sys
 import time
+import os
+import logging
 
 from kafka import KafkaConsumer
 import psycopg2
@@ -14,7 +16,7 @@ kafka_cafile_path = os.path.join(kafka_conf_dir, "ca.pem")
 kafka_certfile_path = os.path.join(kafka_conf_dir, "service.cert")
 kafka_keyfile_path = os.path.join(kafka_conf_dir, "service.key")
 
-psql_urifile_path = os.path.join(kafka_conf_dir, "psql_uri.txt")
+psql_urifile_path = os.path.join(psql_conf_dir, "psql_uri.txt")
 psql_cafile_path = os.path.join(psql_conf_dir, "ca.pem")
 
 
@@ -28,16 +30,17 @@ keep_running = True
 
 def get_kafka_url():
     with open(kafka_urlfile_path, "r") as file:
-        return file.readlines().strip()
+        return file.read().strip()
 
 def get_psql_uri():
     with open(psql_urifile_path, "r") as file:
-        return file.readlines().strip()
+        return file.read().strip()
 
 kafka_url = get_kafka_url()
 psql_uri = get_psql_uri()      
 
 def init_kafka_consumer():
+    logging.info("init_kafka_consumer ...")
     return KafkaConsumer(
         topic_name,
         enable_auto_commit=True,
@@ -50,21 +53,26 @@ def init_kafka_consumer():
         ssl_certfile=kafka_certfile_path,
         ssl_keyfile=kafka_keyfile_path,
     )
+    logging.info("init_kafka_consumer done")
 
 def init_db(cursor):  
+    logging.info("init_db ...")
     table_schema = """
       service_url varchar NOT NULL,
       timestamp timestamptz NOT NULL,
       response_time_millis integer,
       status_code integer,
-      regex_match boolean
+      regex_match boolean,
       PRIMARY KEY(service_url, timestamp)
     """
 
-    creation_sql = "CREATE TABLE heartbeat IF NOT EXISTS (" + table_schema + ");"
+    creation_sql = "CREATE TABLE IF NOT EXISTS heartbeat (" + table_schema + ");"
     cursor.execute(creation_sql);
+    logging.info("init_db done")
     
-def write_heartbeat(cursor, heartbeat_as_dict):
+def write_heartbeat_to_db(cursor, heartbeat_as_dict):
+    logging.info("write_heartbeat_to_db: " + str(heartbeat_as_dict))
+
     service_url          = heartbeat_as_dict["service_url"]
     timestamp            = heartbeat_as_dict["timestamp"]
     response_time_millis = heartbeat_as_dict["response_time_millis"]
@@ -75,16 +83,18 @@ def write_heartbeat(cursor, heartbeat_as_dict):
                "VALUES(%s,%s,%s,%s,%s) ON CONFLICT DO NOTHING;", (service_url, timestamp, response_time_millis, status_code, regex_match))
 
 def run():
+    logging.info(str(sys.argv[0]) + " started")
     with (psycopg2.connect(psql_uri)) as db_conn:
-       cursor = db_conn.cursor(cursor_factory=RealDictCursor)
-       init_db(cursor)
+        cursor = db_conn.cursor(cursor_factory=RealDictCursor)
+        init_db(cursor)
    
         consumer = init_kafka_consumer()
+        logging.info("Begin Kafka polling loop")
         while keep_running:
             raw_msgs = consumer.poll(timeout_ms=kafka_consumer_timeout_millis)        
             for topic, msgs in raw_msgs.items():
                 for msg in msgs:
                     heartbeat = json.loads(msg)
-                    write_heartbeat(cursor, heartbeat)
+                    write_heartbeat_to_db(cursor, heartbeat)
 
 run()
